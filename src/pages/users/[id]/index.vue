0ae28496e364db2b7fc330991d84d26f919d1021
tristar <tristar.2nd@gmail.com>
2024-01-21 17:23:41 +0900

    userの取得方法を変更

--- src/pages/users/[id]/index.vue
+++ src/pages/users/[id]/index.vue
@@ -7,8 +7,10 @@ const route = useRoute()
 const router = useRouter()
 
 const userId = String(route.params.id)
-const { data: user, error, pending } = fetchUser(userId)
-const { isNotFound, isError } = useFetchResponse(user, error)
+const { data, error, pending } = fetchUser(userId)
+const { isNotFound, isError } = useFetchResponse(data, error)
+
+const user = computed(() => data.value?.data)
 
 function handleCancel() {
   router.go(-1)
@@ -27,23 +29,23 @@ function handleCancel() {
       <ElForm ref="formRef" label-width="100" class="flex flex-col gap-3" style="width: 600px">
         <ElFormItem label="ID">
           <p v-if="pending">-</p>
-          <p v-else>{{ user?.data.id }}</p>
+          <p v-else>{{ user?.id }}</p>
         </ElFormItem>
         <ElFormItem label="氏名">
           <p v-if="pending">-</p>
-          <p v-else>{{ user?.data.name }}</p>
+          <p v-else>{{ user?.name }}</p>
         </ElFormItem>
         <ElFormItem label="会社">
           <p v-if="pending">-</p>
-          <p v-else>{{ user?.data.company.name }}</p>
+          <p v-else>{{ user?.company.name }}</p>
         </ElFormItem>
         <ElFormItem label="登録日">
           <p v-if="pending">-</p>
-          <p v-else>{{ formatDate(user?.data.createdAt) }}</p>
+          <p v-else>{{ formatDate(user?.createdAt) }}</p>
         </ElFormItem>
         <ElFormItem label="更新日">
           <p v-if="pending">-</p>
-          <p v-else>{{ formatDate(user?.data.updatedAt) }}</p>
+          <p v-else>{{ formatDate(user?.updatedAt) }}</p>
         </ElFormItem>
         <div class="flex justify-center">
           <ElButton type="default">編集</ElButton>
